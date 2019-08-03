7be5c99d0347c804cb83b927732dd3412b156d49
Will Reichl <willreichl@gmail.com>
2019-08-02 22:12:03 -0400

    Completed assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,21 +13,26 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.projectForm = new FormGroup({
-      projectName: new FormControl(null, [
+      projectName: new FormControl(
+        null,
         Validators.required,
         this.validateProjectNames.bind(this)
-      ])
+      ),
+      email: new FormControl(null, Validators.email),
+      projectStatus: new FormControl('finished')
     });
+    console.log(this.projectForm);
   }
 
   onSubmit() {
-
+    console.log(this.projectForm);
+    console.log(this.projectForm.value);
   }
 
   validateProjectNames(control: FormControl): Promise<any> | Observable<any> {
     const promise = new Promise<any>((resolve, reject) => {
       if (control.value === 'Test') {
-        resolve({ projectNameForbidden: true});
+        resolve({ projectNameForbidden: true });
       } else {
         resolve(null);
       }
